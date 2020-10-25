caac6e413d0b6a43ffaee4624d1b084559918dfe
Hamza-Rashed <hamzarashed503@gmail.com>
2020-10-25 15:53:05 +0300

    d

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 'use strict';
 
-require('dotenv').config();
-const express = require('express');
-const app = express();
-
-const port = process.env.PORT || 3000;
-
+const
+    express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    locationData = require('./data/location.json'),
+    weatherData = require('./data/weather.json');
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello');
 });
 
+app.get('/location',(req,res)=>{
+  let location = new Location(locationData)
+  res.json(location)
+})
 
-app.use('*', (req, res) => response.send('Sorry, that route does not exist.'));
+// app.get('/weather',(req,res)=>{
+//   let weather = new Weather(weatherData)
+//   res.json(weather)
+// })
+
+function Location(information) {
+this.search_query = information[0].display_name
+this.icon = information[0].icon
+this.lat = information[0].lat
+this.lon = information[0].lon 
+}
+
+// function Weather(information) {
+//   this.search_query = information[0].display_name
+//   this.icon = information[0].icon
+//   this.lat = information[0].lat
+//   this.lon = information[0].lon 
+//   }
+
+app.use('*', (req, res) => res.send('Sorry, that route does not exist.'));
 
 app.listen(port,() => console.log('server is running'));
