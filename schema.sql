1bd18986513bf0eaf140aaf96820bb37612268d4
Hamza-Rashed <hamzarashed503@gmail.com>
2020-10-27 18:14:48 +0300

    done lab 8

---
+++ schema.sql
@@ -0,0 +1,32 @@
+
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS weather;
+DROP TABLE IF EXISTS trails;
+
+CREATE TABLE location(
+    ID SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    display_name VARCHAR(255),
+    lat VARCHAR(255),
+    lon VARCHAR(255)
+);
+CREATE TABLE weather(
+    ID SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    forecast VARCHAR(255),
+    time VARCHAR(255)
+);
+CREATE TABLE trails(
+    ID SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    name VARCHAR(255),
+    location VARCHAR(255),
+    length VARCHAR(255),
+    stars VARCHAR(255),
+    star_votes VARCHAR(255),
+    summary VARCHAR(255),
+    trail_url VARCHAR(255),
+    conditions VARCHAR(255),
+    condition_date VARCHAR(255),
+    condition_time VARCHAR(255)
+);
